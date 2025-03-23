eebe3402c83399e3892fd9efa6cef28b643aca91
fredieposh <maor1234@gmail.com>
2025-03-24 00:37:42 +0200

    Add column divs to the grid

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ for (let i = 0; i < 16; i++) {
     containerDiv = document.querySelector("#container");
     containerDiv.appendChild(gridRowDiv);
 }
+
+gridRowsList = document.querySelectorAll(".gridRow");
+gridRowsList.forEach(gridRow => {
+    for (let i = 0; i < 16; i++) {
+        gridColDiv = document.createElement("div");
+        gridColDiv.classList.add("gridCol");
+        gridRow.appendChild(gridColDiv);
+    }
+})
