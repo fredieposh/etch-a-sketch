6837e3eb113b9ce272fd691ea8ed5a8f81e72c2f
fredieposh <maor1234@gmail.com>
2025-03-25 23:07:13 +0200

    Add change grid button event listener
    To add ability of changing the grid size for the user.

--- script.js
+++ script.js
@@ -1,32 +1,54 @@
 // Creating the grid
-function createGrid(n=16){
+function createGrid(n = 16) {
     for (let i = 0; i < n; i++) {
-        gridRowDiv = document.createElement("div");
+        const gridRowDiv = document.createElement("div");
         gridRowDiv.classList.add("gridRow");
-        
-        containerDiv = document.querySelector("#container");
+
+        const containerDiv = document.querySelector("#container");
         containerDiv.appendChild(gridRowDiv);
     }
-    
-    gridRowsList = document.querySelectorAll(".gridRow");
+
+    const gridRowsList = document.querySelectorAll(".gridRow");
     gridRowsList.forEach(gridRow => {
         for (let i = 0; i < n; i++) {
             gridColDiv = document.createElement("div");
             gridColDiv.classList.add("gridCol");
             gridRow.appendChild(gridColDiv);
         }
-    })
+    });
+}
+
+function removeGrid() {
+    const containerDiv = document.querySelector("#container");
+    while (containerDiv.firstChild) {
+        containerDiv.firstChild.remove();
+    }
+}
+
+function paintCells() {
+    const gridCells = document.querySelectorAll(".gridCol");
+    gridCells.forEach(cell => {
+        cell.addEventListener('mouseenter', (e) => {
+            e.target.classList.add("cellColor");
+        });
+        cell.addEventListener('mouseout', (e) => {
+            e.preventDefault();
+        });
+    });
 }
 
 createGrid();
+paintCells();
 
-// Paint the cells
-gridCells = document.querySelectorAll(".gridCol");
-gridCells.forEach(cell => {
-    cell.addEventListener('mouseenter', (e) => {
-        e.target.classList.add("cellColor");
-    });
-    cell.addEventListener('mouseout', (e) => {
-        e.preventDefault();
-    });
-})
+// Button click triggers input
+
+const changeGridSizeButton = document.querySelector("button");
+changeGridSizeButton.addEventListener('click', () => {
+    do {
+        gridSize = +prompt("Please enter grid sizer between 16 and 100");
+    } while ((isNaN(gridSize)) || (gridSize < 16) || (gridSize > 100));
+
+    removeGrid();
+    createGrid(gridSize);
+    paintCells();
+});
